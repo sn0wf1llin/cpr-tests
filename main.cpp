4e9ed07a9d313b9b35ec5e0645772417978af974
sn0wf1llin <sn0wf1llin@gmail.com>
2019-07-01 17:30:38 +0300

    ;

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "string"
+#include <stdio.h>
+#include <cpr/cpr.h>
+
+
+using namespace std;
+
+int mypost(string url){
+
+    return 0;
+
+}
+
+int myget(string url){
+
+    return 0;
+}
+
+int main() {
+    string url;
+
+    url = "";
+    int ger_result = myget(url);
+    int post_result = mypost(url, data);
+
+    cout << "Hello, World!" << endl;
+    return 0;
+}
