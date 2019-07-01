d6249bc04df3b1fa2e81676fbce9b23659a5ec47
sn0wf1llin <sn0wf1llin@gmail.com>
2019-07-01 17:48:37 +0300

    ;

--- main.cpp
+++ main.cpp
@@ -2,25 +2,35 @@
 #include "string"
 #include <stdio.h>
 #include <cpr/cpr.h>
+#include <map>
+#include <cpr/curlholder.h>
 
 
 using namespace std;
 
-int mypost(string url){
+int mypost(string url, std::map<std::string,std::string> data){
 
     return 0;
 
 }
 
-int myget(string url){
+int myget(std::string url){
+    auto urlObject = cpr::Url(url);
+    auto r = cpr::Get(urlObject);
+    cout << r.text << endl;
 
-    return 0;
+    return r.status_code;
 }
 
 int main() {
-    string url;
+    std::string url;
 
-    url = "";
+    url = "http://www.example.net";
+    std::map<std::string, std::string> data;
+
+    data["user"] = "test";
+    data["password"] = "test111";
+
     int ger_result = myget(url);
     int post_result = mypost(url, data);
 
