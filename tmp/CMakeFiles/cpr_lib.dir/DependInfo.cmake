7bb6b68dcb724181618ab4267eb61c316b3bef3f
sn0wf1llin <sn0wf1llin@gmail.com>
2019-07-02 15:09:14 +0300

    cmake; get works;

---
+++ tmp/CMakeFiles/cpr_lib.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nn5h/CLionProjects/undetected/include/libs/cpr/cpr/auth.cpp" "/home/nn5h/CLionProjects/undetected/tmp/CMakeFiles/cpr_lib.dir/home/nn5h/CLionProjects/undetected/include/libs/cpr/cpr/auth.cpp.o"
+  "/home/nn5h/CLionProjects/undetected/include/libs/cpr/cpr/cookies.cpp" "/home/nn5h/CLionProjects/undetected/tmp/CMakeFiles/cpr_lib.dir/home/nn5h/CLionProjects/undetected/include/libs/cpr/cpr/cookies.cpp.o"
+  "/home/nn5h/CLionProjects/undetected/include/libs/cpr/cpr/cprtypes.cpp" "/home/nn5h/CLionProjects/undetected/tmp/CMakeFiles/cpr_lib.dir/home/nn5h/CLionProjects/undetected/include/libs/cpr/cpr/cprtypes.cpp.o"
+  "/home/nn5h/CLionProjects/undetected/include/libs/cpr/cpr/digest.cpp" "/home/nn5h/CLionProjects/undetected/tmp/CMakeFiles/cpr_lib.dir/home/nn5h/CLionProjects/undetected/include/libs/cpr/cpr/digest.cpp.o"
+  "/home/nn5h/CLionProjects/undetected/include/libs/cpr/cpr/error.cpp" "/home/nn5h/CLionProjects/undetected/tmp/CMakeFiles/cpr_lib.dir/home/nn5h/CLionProjects/undetected/include/libs/cpr/cpr/error.cpp.o"
+  "/home/nn5h/CLionProjects/undetected/include/libs/cpr/cpr/multipart.cpp" "/home/nn5h/CLionProjects/undetected/tmp/CMakeFiles/cpr_lib.dir/home/nn5h/CLionProjects/undetected/include/libs/cpr/cpr/multipart.cpp.o"
+  "/home/nn5h/CLionProjects/undetected/include/libs/cpr/cpr/parameters.cpp" "/home/nn5h/CLionProjects/undetected/tmp/CMakeFiles/cpr_lib.dir/home/nn5h/CLionProjects/undetected/include/libs/cpr/cpr/parameters.cpp.o"
+  "/home/nn5h/CLionProjects/undetected/include/libs/cpr/cpr/payload.cpp" "/home/nn5h/CLionProjects/undetected/tmp/CMakeFiles/cpr_lib.dir/home/nn5h/CLionProjects/undetected/include/libs/cpr/cpr/payload.cpp.o"
+  "/home/nn5h/CLionProjects/undetected/include/libs/cpr/cpr/proxies.cpp" "/home/nn5h/CLionProjects/undetected/tmp/CMakeFiles/cpr_lib.dir/home/nn5h/CLionProjects/undetected/include/libs/cpr/cpr/proxies.cpp.o"
+  "/home/nn5h/CLionProjects/undetected/include/libs/cpr/cpr/session.cpp" "/home/nn5h/CLionProjects/undetected/tmp/CMakeFiles/cpr_lib.dir/home/nn5h/CLionProjects/undetected/include/libs/cpr/cpr/session.cpp.o"
+  "/home/nn5h/CLionProjects/undetected/include/libs/cpr/cpr/ssl_options.cpp" "/home/nn5h/CLionProjects/undetected/tmp/CMakeFiles/cpr_lib.dir/home/nn5h/CLionProjects/undetected/include/libs/cpr/cpr/ssl_options.cpp.o"
+  "/home/nn5h/CLionProjects/undetected/include/libs/cpr/cpr/timeout.cpp" "/home/nn5h/CLionProjects/undetected/tmp/CMakeFiles/cpr_lib.dir/home/nn5h/CLionProjects/undetected/include/libs/cpr/cpr/timeout.cpp.o"
+  "/home/nn5h/CLionProjects/undetected/include/libs/cpr/cpr/util.cpp" "/home/nn5h/CLionProjects/undetected/tmp/CMakeFiles/cpr_lib.dir/home/nn5h/CLionProjects/undetected/include/libs/cpr/cpr/util.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/lib/x86_64-linux-gnu"
+  "/home/nn5h/CLionProjects/undetected/include/libs"
+  "/home/nn5h/CLionProjects/undetected/include/libs/cpr"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ tmp/CMakeFiles/cpr_lib.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libcpr_lib.a"
+)
